478e6e65f325410c795a91d73766b3a069b98244
John Taggart <john@MacBook-Pro.connect>
2020-06-19 20:19:32 +0100

    monsterController added

--- src/main/java/com/example/codeclan/DwarvesOfKurak/controller/MonsterController.java
+++ src/main/java/com/example/codeclan/DwarvesOfKurak/controller/MonsterController.java
@@ -1,4 +1,23 @@
 package com.example.codeclan.DwarvesOfKurak.controller;
 
+import com.example.codeclan.DwarvesOfKurak.models.Character;
+import com.example.codeclan.DwarvesOfKurak.models.Monster;
+import com.example.codeclan.DwarvesOfKurak.repository.CharacterRepository;
+import com.example.codeclan.DwarvesOfKurak.repository.MonsterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
 public class MonsterController {
+
+    @Autowired
+    MonsterRepository monsterRepository;
+
+    @GetMapping(value = "/api/monsters")
+    public ResponseEntity<List<Monster>> getAllMonsters(){
+        return new ResponseEntity<>(monsterRepository.findAll(), HttpStatus.OK);
+    }
 }
