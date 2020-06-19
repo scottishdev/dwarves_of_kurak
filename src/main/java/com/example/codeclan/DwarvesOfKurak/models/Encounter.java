b1cf88d7e65b2940a41e80e7c35287b4f6aadb62
User <user@Users-MacBook-Pro.local>
2020-06-19 21:57:42 +0100

    encounter model created
    Signed-off-by: User <user@Users-MacBook-Pro.local>

---
+++ src/main/java/com/example/codeclan/DwarvesOfKurak/models/Encounter.java
@@ -0,0 +1,66 @@
+package com.example.codeclan.DwarvesOfKurak.models;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "encounters")
+public class Encounter {
+
+    @JsonBackReference
+    @OneToMany(mappedBy = "encounter", fetch = FetchType.LAZY)
+    private List<Monster> monsters;
+
+    @Column(name = "inventory")
+    private List<Item> inventory;
+
+    @Column(name = "coinPurse")
+    private int coinPurse;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Encounter(List<Monster> monsters, List<Item> inventory, int coinPurse) {
+        this.monsters = monsters;
+        this.inventory = inventory;
+        this.coinPurse = coinPurse;
+    }
+
+    public Encounter() {
+    }
+
+    public List<Monster> getMonsters() {
+        return monsters;
+    }
+
+    public void setMonsters(List<Monster> monsters) {
+        this.monsters = monsters;
+    }
+
+    public List<Item> getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(List<Item> inventory) {
+        this.inventory = inventory;
+    }
+
+    public int getCoinPurse() {
+        return coinPurse;
+    }
+
+    public void setCoinPurse(int coinPurse) {
+        this.coinPurse = coinPurse;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
