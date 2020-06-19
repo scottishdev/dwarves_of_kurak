dfba578dd2ed9dcc87754d42bcff37106febca76
User <user@Users-MacBook-Pro.local>
2020-06-19 13:51:30 +0100

    first commit
    Signed-off-by: User <user@Users-MacBook-Pro.local>

---
+++ src/main/java/com/example/codeclan/DwarvesOfKurak/DwarvesOfKurakApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.DwarvesOfKurak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwarvesOfKurakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DwarvesOfKurakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codeclan/DwarvesOfKurak/DwarvesOfKurakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.DwarvesOfKurak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwarvesOfKurakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
