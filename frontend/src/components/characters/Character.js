878754dddf812e2b95b2de4717f7b2e52e77a210
John <john@MacBook-Pro.connect>
2020-06-20 16:38:16 +0100

    front end updated

---
+++ frontend/src/components/characters/Character.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+const Character = (character) => {
+  return (
+    // <p>{character.name}</p>
+    <p>Character stuff goes here</p>
+  )
+
+}
+
+export default Character;
