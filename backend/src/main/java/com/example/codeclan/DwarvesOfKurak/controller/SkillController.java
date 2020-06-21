c9af211de0ac570ec2fe8c193b4203ada312ad4f
taggartj1985 <taggartj1985@gmail.com>
2020-06-21 20:28:08 +0100

    skills crud added

--- backend/src/main/java/com/example/codeclan/DwarvesOfKurak/controller/SkillController.java
+++ backend/src/main/java/com/example/codeclan/DwarvesOfKurak/controller/SkillController.java
@@ -1,13 +1,13 @@
 package com.example.codeclan.DwarvesOfKurak.controller;
 
 
+import com.example.codeclan.DwarvesOfKurak.models.Job;
 import com.example.codeclan.DwarvesOfKurak.models.Skill;
 import com.example.codeclan.DwarvesOfKurak.repository.SkillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +20,28 @@ public class SkillController {
     @GetMapping(value ="/api/skills")
     public ResponseEntity<List<Skill>> getAllSkills(){
         return new ResponseEntity<>(skillRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value="/api/skills/{id}")
+    public ResponseEntity getSkill(@PathVariable Long id){
+        return new ResponseEntity<>(skillRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value="/api/skills")
+    public ResponseEntity<Skill> createSkill(@RequestBody Skill skill){
+        skillRepository.save(skill);
+        return new ResponseEntity<>(skill, HttpStatus.CREATED);
+    }
+
+    @PutMapping (value="/api/skills/{id}")
+    public ResponseEntity<Skill> updateSkill(@RequestBody Skill skill){
+        skillRepository.save(skill);
+        return new ResponseEntity<>(skill, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value="/api/skills/{id}")
+    public ResponseEntity<Long> deleteSkill(@PathVariable Long id){
+        skillRepository.deleteById(id);
+        return new ResponseEntity<>(id, HttpStatus.OK);
     }
 }
