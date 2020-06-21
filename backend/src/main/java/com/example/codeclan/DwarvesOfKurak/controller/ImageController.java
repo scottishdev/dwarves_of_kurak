033c1ec01a7d95dc514b23fe44035db860edcb87
Teo <43215755+teokigi@users.noreply.github.com>
2020-06-21 11:53:25 +0100

    allowed searching of images in assets

--- backend/src/main/java/com/example/codeclan/DwarvesOfKurak/controller/ImageController.java
+++ backend/src/main/java/com/example/codeclan/DwarvesOfKurak/controller/ImageController.java
@@ -5,18 +5,36 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
 import org.springframework.util.StreamUtils;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(value="/api/assets")
 public class ImageController {
-    @RequestMapping(value = "api/images/beach", method = RequestMethod.GET,
-            produces = MediaType.IMAGE_JPEG_VALUE)
+    @GetMapping(value = "/jpg", produces = MediaType.IMAGE_JPEG_VALUE)
 
-    public void getImage(HttpServletResponse response) throws IOException {
+    public void getImage(HttpServletResponse response, @RequestParam(name="image") String imageName) throws IOException {
 
-        ClassPathResource imgFile = new ClassPathResource("images/Chk1VFQ.jpg");
+        ClassPathResource imgFile = new ClassPathResource("images/" + imageName +".jpg");
+
+        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+        StreamUtils.copy(imgFile.getInputStream(), response.getOutputStream());
+    }
+
+    @GetMapping(value = "/png", produces = MediaType.IMAGE_PNG_VALUE)
+
+    public void getImage2(HttpServletResponse response, @RequestParam(name="image") String imageName) throws IOException {
+
+        ClassPathResource imgFile = new ClassPathResource("images/" + imageName +".png");
+
+        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+        StreamUtils.copy(imgFile.getInputStream(), response.getOutputStream());
+    }
+
+    @GetMapping(value = "/gif", produces = MediaType.IMAGE_GIF_VALUE)
+
+    public void getImage3(HttpServletResponse response, @RequestParam(name="image") String imageName) throws IOException {
+
+        ClassPathResource imgFile = new ClassPathResource("images/" + imageName +".gif");
 
         response.setContentType(MediaType.IMAGE_JPEG_VALUE);
         StreamUtils.copy(imgFile.getInputStream(), response.getOutputStream());
