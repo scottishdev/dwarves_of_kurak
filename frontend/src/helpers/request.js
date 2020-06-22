10270f39adf6f2022a15e328c728d61d745142d1
Teo <43215755+teokigi@users.noreply.github.com>
2020-06-22 09:52:52 +0100

    helper code for frontend to backend communication

--- frontend/src/helpers/request.js
+++ frontend/src/helpers/request.js
@@ -1,9 +1,34 @@
 class Request {
 
-  get(url){
-    return fetch(url)
-    .then(res => res.json())
-  }
+    get(url){
+        return fetch(url)
+        .then(res => res.json())
+    }
+
+    show(url){
+        return fetch(url)
+        .then(res =>res.json())
+    }
+
+    updateUser(id, payload)
+   {
+     return fetch(baseURL + id,
+                 {method:'PUT',
+                  body: JSON.stringify(payload),
+                  headers: { 'Content-Type': 'application/json' }
+                 })
+                 .then(res => res.json())
+  },
+postUser(payload)
+ {
+    return fetch(baseURL,
+       {
+            method: 'POST',
+            body: JSON.stringify(payload),
+            headers: { 'Content-Type': 'application/json' }
+       })
+       .then(res => res.json())
+  },
 }
 
 
