b190d89af74067938c1dc888c1719b6cfd2ac2b7
Teo <43215755+teokigi@users.noreply.github.com>
2020-06-22 13:11:40 +0100

    party selected

--- frontend/src/containers/PartyCharacterSelect.jsx
+++ frontend/src/containers/PartyCharacterSelect.jsx
@@ -9,14 +9,26 @@ const PartyScreenContainer = (props) => {
         return <option value={character.id}> {character.name}</option>
     })
 
-  return (
-    <div className="party_character_select_box">
-        <p>characterBox</p>
-        <select onChange={(id)=>props.onHandleChange(id,props.key)} defaultValue="default">
-            <option disabled value="default">Choose a playable character</option>
-            {options}
-        </select>
-    </div>
-  )
+    if(!props.character){
+        return (
+            <div className="party_character_select_box">
+                
+                <p>characterBox</p>
+                
+                <select onChange={(id)=>props.onHandleChange(id.target.value)} defaultValue="default">
+                    <option disabled value="default">Choose a playable character</option>
+                    {options}
+                </select>
+
+            </div>
+        )
+    } else{
+        return (
+            <div className="party_character_select_box">
+                <p> character selected</p>
+                <p>{props.character.name}</p>
+            </div>
+        )
+    }
 }
 export default PartyScreenContainer;
--- frontend/src/containers/PartySelectContainer.jsx
+++ frontend/src/containers/PartySelectContainer.jsx
@@ -16,75 +16,52 @@ class PartySelectContainer extends Component {
         this.handlePartyChangeThree = this.handlePartyChangeThree.bind(this);
       }
 
-      handlePartyChangeOne(id){
-          const character = this.state.characters.find(char=>char.id === id)
-          switch(key){
-                case 1:
-                    this.setState({selectedCharacterOne:character})
-                    break
-                case 2:
-                    this.setState({selectedCharacterTwo:character})
-                    break
-                case 3:
-                    this.setState({selectedCharacterThree:character})
-                    break
-          }
+    handlePartyChangeOne(id){
+        const char = this.state.characters.find(character=>character.id == id);
+            this.setState({selectedCharacterOne:char})
         }
-        handlePartyChangeTwo(id){
-            const character = this.state.characters.find(char=>char.id === id)
-            switch(key){
-                  case 1:
-                      this.setState({selectedCharacterOne:character})
-                      break
-                  case 2:
-                      this.setState({selectedCharacterTwo:character})
-                      break
-                  case 3:
-                      this.setState({selectedCharacterThree:character})
-                      break
-            }
+        
+    handlePartyChangeTwo(id){
+        const character = this.state.characters.find(char=>char.id == id)
+            this.setState({selectedCharacterTwo:character})
+    }
           
-  
-        }
-        handlePartyChangeThree(id){
-            const character = this.state.characters.find(char=>char.id === id)
-            switch(key){
-                  case 1:
-                      this.setState({selectedCharacterOne:character})
-                      break
-                  case 2:
-                      this.setState({selectedCharacterTwo:character})
-                      break
-                  case 3:
-                      this.setState({selectedCharacterThree:character})
-                      break
-            }
-          
-  
-        }
+    handlePartyChangeThree(id){
+        const character = this.state.characters.find(char=>char.id == id)
+            this.setState({selectedCharacterThree:character})
+    }
     
-  componentDidMount() {
-    const request = new Request();
+    componentDidMount() {
+        const request = new Request();
 
-    request.get('/characters')
-    .then(data => this.setState({characters:data}))
-  }
+        request.get('/characters')
+        .then(data => this.setState({characters:data}))
+    }
     
 
-render(){
-      return (
-        <div className="party_setup">
-            <div className="page_heading">
-                <h3>Party Setup</h3>
+    render(){
+        if(!this.state.selectedCharacterOne||!this.state.selectedCharacterTwo||!this.state.selectedCharacterThree){
+        return (
+            <div className="party_setup">
+                <div className="page_heading">
+                    <h3>Party Setup</h3>
+                </div>
+                <div className="party_character_boxes">
+                <PartyCharacterSelect characters={this.state.characters} character={this.state.selectedCharacterOne} onHandleChange={this.handlePartyChangeOne}/>
+                <PartyCharacterSelect characters={this.state.characters} character={this.state.selectedCharacterTwo}  onHandleChange={this.handlePartyChangeTwo} />
+                <PartyCharacterSelect characters={this.state.characters} character={this.state.selectedCharacterThree}  onHandleChange={this.handlePartyChangeThree}/>
+                </div>
             </div>
-            <div className="party_character_boxes">
-            <PartyCharacterSelect characters={this.state.characters} onHandleChange={this.handlePartyChangeOne}/>
-            <PartyCharacterSelect characters={this.state.characters} onHandleChange={this.handlePartyChangeTwo} />
-            <PartyCharacterSelect characters={this.state.characters} onHandleChange={this.handlePartyChangeThree}/>
-            </div>
-        </div>
-    )
-      }
+            )
+        } else{
+            return(
+                <div className="party_setup">
+                    party selected
+                </div>
+            )
+        }
+    }
+
 }
 
 export default PartySelectContainer;
