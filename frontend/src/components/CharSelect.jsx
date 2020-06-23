d5bf491be6f645694abf7a2b87d90ca494bc7968
User <user@Users-MacBook-Pro.local>
2020-06-23 21:45:26 +0100

    finished unique character select dropdown
    Signed-off-by: User <user@Users-MacBook-Pro.local>

--- frontend/src/components/CharSelect.jsx
+++ frontend/src/components/CharSelect.jsx
@@ -2,32 +2,42 @@ import React from 'react';
 
 const CharSelect = (props) => {
 
-    if(!props.characters) return null
-        
-    const options = props.characters.map((character, index) =>{
-        return <option value={character.id} key={index}> {character.name}</option>
-    })
+  if(!props.characters) return null
 
-    if(!props.character){
-        return (
-            <div className="party_character_select_box">
-                
-                <p>characterBox</p>
-                
-                <select onChange={(id)=>props.onHandleChange(id.target.value)} defaultValue="default">
-                    <option disabled value="default">Choose a playable character</option>
-                    {options}
-                </select>
-
-            </div>
-        )
-    } else{
-        return (
-            <div className="party_character_select_box">
-                <p> character selected</p>
-                <p>{props.character.name}</p>
-            </div>
-        )
+  const options = props.characters.map((character, index) => {
+    if (
+      character.selectedDropdown === undefined ||
+      props.index === character.selectedDropdown
+    ) {
+      return (
+        <option value={character.name} key={index}>
+        {" "}
+        {character.name}
+        </option>
+      );
     }
+  });
+
+  if(!props.character){
+    return (
+      <div className="party_character_select_box">
+
+      <p>characterBox</p>
+
+      <select onChange={(id)=>props.onHandleChange(id.target.value)} defaultValue="default">
+      <option disabled value="default">Choose a playable character</option>
+      {options}
+      </select>
+
+      </div>
+    )
+  } else{
+    return (
+      <div className="party_character_select_box">
+      <p> character selected</p>
+      <p>{props.character.name}</p>
+      </div>
+    )
+  }
 }
 export default CharSelect;
--- frontend/src/containers/PartySelect.jsx
+++ frontend/src/containers/PartySelect.jsx
@@ -3,51 +3,53 @@ import CharSelect from '../components/CharSelect';
 
 const PartySelect = (props)=>{
 
-    const [selChar,setSelChar] = useState({party:[],loopcount:[0,1,2]});
-    const i = [0,1,2]
-    
-    function handlePartyChange(id,index){
-        console.log("change party member " + index);
-        const character = props.characterAssets.find(character=>character.id == id);
-        const party = selChar.party
-        party[index] = character
-        setSelChar({party:party})
-    }
+  const [selChar,setSelChar] = useState({party:[],loopcount:[0,1,2]});
+  const i = [0,1,2]
 
-    const partySelectorComponents = i.map(index=>{
-            return (
-                <CharSelect 
-                    characters={props.characterAssets}
-                    character={selChar.party[index]}
-                    key={index}
-                    onHandleChange={char_id=>handlePartyChange(char_id,index)}
-                 />
-                
-            )
-          
-        })
+  function handlePartyChange(id, index) {
+    console.log("change party member " + index);
+    const character = props.characterAssets.find((character) => character.name == id);
+    character.selectedDropdown = index;
+    const party = selChar.party;
+    party[index] = character;
+    setSelChar({ party: party });
+  }
 
-    if(selChar.party.length<3){
-        return (
-            <div className="party_setup">
-                {console.log("rendering partySelect Container")}
-                
-                <div className="page_heading">
-                    <h3>Party Setup</h3>
-                </div>
-                <div className="party_character_boxes">
-                    {partySelectorComponents}
-                </div>
-            </div>
-        )
-    } else{
-        return(
-            <div className="party_setup">
-                <p>party setup complete</p>
-                <button onClick={props.setPartyHandler} >Start Game</button>
-            </div>
-        )
-    }
+
+  const partySelectorComponents = i.map(index=>{
+    return (
+      <CharSelect
+      characters={props.characterAssets}
+      character={selChar.party[index]}
+      key={index}
+      onHandleChange={char_id=>handlePartyChange(char_id,index)}
+      />
+
+    )
+
+  })
+
+  if(selChar.party.length<3){
+    return (
+      <div className="party_setup">
+      {console.log("rendering partySelect Container")}
+
+      <div className="page_heading">
+      <h3>Party Setup</h3>
+      </div>
+      <div className="party_character_boxes">
+      {partySelectorComponents}
+      </div>
+      </div>
+    )
+  } else{
+    return(
+      <div className="party_setup">
+      <p>party setup complete</p>
+      <button onClick={props.setPartyHandler} >Start Game</button>
+      </div>
+    )
+  }
 }
 
 
