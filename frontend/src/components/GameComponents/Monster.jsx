b090e4222d557d0675054b833eb76b711882ea8b
Teo <43215755+teokigi@users.noreply.github.com>
2020-06-23 17:59:24 +0100

    can view player stats

--- frontend/src/components/GameComponents/Monster.jsx
+++ frontend/src/components/GameComponents/Monster.jsx
@@ -1,22 +1,19 @@
 import React from 'react';
 
+const Monster = (props) => {
 
+    return (
+        <div className="game_screen_monster">
+            {console.log("show monster")}
+            {console.log(props.monster[1])}
+            <span>Name:{props.monster[1].name}</span><br />
+            <span>Race:{props.monster[1].race}</span><br />
+            <span>HP:{props.monster[1].hitPoints}</span><br />
+            <span>MP:{props.monster[1].manaPoints}</span>
 
-const Monster = ({monster}) => {
-
-  if(!monster){
-    return "no gas..."
-  }
-  return (
-    <div className="game_screen_monster">
-      <h3>Name:{monster.name}</h3>
-      <p>Race:{monster.race}</p>
-      <p>HP:{monster.hitPoints}</p>
-      <p>MP:{monster.manaPoints}</p>
-      <p>Strength:{monster.strength}</p>
-      <p>Intelligence:{monster.intelligence}</p>
-    </div>
-  )
+            
+        </div>
+    )
 
 }
 
--- frontend/src/containers/Game.jsx
+++ frontend/src/containers/Game.jsx
@@ -1,61 +1,62 @@
 import React, { useState, useEffect } from 'react';
 import BattleLog from '../components/GameComponents/BattleLog';
 import Monster from '../components/GameComponents/Monster';
+import Character from '../components/GameComponents/Character';
 import Request from '../helpers/request';
 
-const GameContainer =  (props)=>{
+const Game = (props)=>{
     
     // function binds here
-    const [monster,setMonster] = useState();
+    const [monsters,setMonsters] = useState(null);
+    const [character,setCharacter] = useState(null);
     const request = new Request;
 
     // function creation here
     useEffect(()=>{
         getPost()
-
-        
+        getParty()
+         
     },[])
     
     const getPost = ()=>{
         request.get('monsters')
-        .then(data=>setMonster({Monster:data}))
+        .then(data=>setMonsters(data))
     }
 
-    if (!monster){
-        return null;
+    const getParty = ()=>{
+        request.get('party')
+        .then(data=>setCharacter(data))
     }
-    else{
-        return (
-            <div className="game_screen">
-                {console.log("rendering game")}
-                <div className="game_screen_top">
-                    <BattleLog />
-                    <Monster monster={monster[0]}/>
-                    <div className="game_screen_sprites">
-                        <img src="http://localhost:8080/api/assets/gif?image=slime" width="50px" height="50px"/>
-                        <img src="http://localhost:8080/api/assets/gif?image=slime" width="25px" height="25px"/>
-                        <img src="http://localhost:8080/api/assets/gif?image=slime" width="25px" height="25px"/>
-                        <img src="http://localhost:8080/api/assets/gif?image=slime" width="50px" height="50px"/>
-                        <img src="http://localhost:8080/api/assets/gif?image=slime" width="70px" height="70px" />
-                        <img src="http://localhost:8080/api/assets/gif?image=slime" width="20px" height="20px" />
-                        <img src="http://localhost:8080/api/assets/gif?image=slime" width="50px" height="50px"/>
-                        <img src="http://localhost:8080/api/assets/gif?image=slime" width="100px" height="100px"/>
-                    </div>
-                </div>
-                <div className="game_screen_bottom">
-                    <div className="game_screen_player_stats">
-                        player information
-                    </div>
-                    <div className="game_screen_player_commands">
-                        player commands
-                    </div>
-                    <div className="game_screen_minimap">
-                        minimap goes here
-                    </div>
+
+    if(!character) return null
+    return (
+        <div className="game_screen">
+            {console.log("rendering game")}
+            <div className="game_screen_top">
+                <BattleLog />
+                <Monster monster={monsters}/>
+                <div className="game_screen_sprites">
+                    <img src="http://localhost:8080/api/assets/gif?image=slime" width="50px" height="50px"/>
+                    <img src="http://localhost:8080/api/assets/gif?image=slime" width="25px" height="25px"/>
+                    <img src="http://localhost:8080/api/assets/gif?image=slime" width="25px" height="25px"/>
+                    <img src="http://localhost:8080/api/assets/gif?image=slime" width="50px" height="50px"/>
+                    <img src="http://localhost:8080/api/assets/gif?image=slime" width="70px" height="70px" />
+                    <img src="http://localhost:8080/api/assets/gif?image=slime" width="20px" height="20px" />
+                    <img src="http://localhost:8080/api/assets/gif?image=slime" width="50px" height="50px"/>
+                    <img src="http://localhost:8080/api/assets/gif?image=slime" width="100px" height="100px"/>
                 </div>
             </div>
-        )
-    }    
+            <div className="game_screen_bottom">
+                <Character character={character}/>
+                <div className="game_screen_player_commands">
+                    player commands
+                </div>
+                <div className="game_screen_minimap">
+                    minimap goes here
+                </div>
+            </div>
+        </div>
+    )
 
 }
-export default GameContainer;
+export default Game;
