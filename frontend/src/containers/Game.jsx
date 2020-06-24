7e7301a8802c091b30428f7fbd54c4b441a24fb1
Teo <43215755+teokigi@users.noreply.github.com>
2020-06-24 10:47:14 +0100

    fight logic into battle log complete

--- frontend/src/containers/Game.jsx
+++ frontend/src/containers/Game.jsx
@@ -8,8 +8,8 @@ import Request from '../helpers/request';
 const Game = (props)=>{
     
     // function binds here
-    const [monsters,setMonsters] = useState(null);
-    const [character,setCharacter] = useState(null);
+    const [enemy,setEnemy] = useState(null);
+    const [player,setPlayer] = useState(null);
     const [battleLog,setBattleLog] = useState({log:["Battle Log"]});
     const request = new Request;
 
@@ -21,27 +21,55 @@ const Game = (props)=>{
     
     const getPost = ()=>{
         request.get('monsters')
-        .then(data=>setMonsters(data[1]))
+        .then(data=>setEnemy({monster:data[1]}))
     }
 
     const getParty = ()=>{
         request.get('party')
-        .then(data=>setCharacter(data[0].characters[0]))
+        .then(data=>setPlayer({character:data[0].characters[0]}))
     }
 
     function attack(){
         console.log("updateBattleLog");
-        const turn_one = "" + character.name + " attacks " + monsters.name + " for " + character.strength + " damage."
-        damageMonster(character.strength)
-        const turn_two = "" + monsters.name + " attacks " + character.name + " for " + monsters.strength + " damage."
-        damagePlayer(monsters.strength)
-        // const updateCharacter = character
-        //logic => player attack minus monster hp and render update.
-        //logic => wait 2 sec
-        //log => monstername attacks player for # damage.
-        //logic => monster attack minus player hp and render update.
-        const newLog = updateBattleEvents(turn_one)
-        setBattleLog({log:newLog})
+        
+        if(player.character.hitPoints > 0){
+            if(enemy.monster.hitPoints > 0){
+                damageMonster(player.character.strength)
+                const turn_one = "" + player.character.name + " attacks " + enemy.monster.name + " for " + player.character.strength + " damage."
+                const newLog = updateBattleEvents(turn_one)
+                setBattleLog({log:newLog})
+            }
+            else{
+                const turn_one = "quit beating a dead horse"
+                const newLog = updateBattleEvents(turn_one)
+                setBattleLog({log:newLog})
+            }
+        }
+        else{
+            const turn_one = "Player is too dead to attack."
+            const newLog = updateBattleEvents(turn_one)
+            setBattleLog({log:newLog})
+        }
+
+        if(enemy.monster.hitPoints > 0){
+            if(player.character.hitPoints > 0){
+                damagePlayer(enemy.monster.strength)
+                const turn_two = "" + enemy.monster.name + " attacks " + player.character.name + " for " + enemy.monster.strength + " damage."
+                const newLog2 = updateBattleEvents(turn_two)
+                setBattleLog({log:newLog2})
+            }
+            else{
+                const turn_two = "monster chomps on the players corpse"
+                const newLog2 = updateBattleEvents(turn_two)
+                setBattleLog({log:newLog2})
+            }
+        }
+        else{
+            const turn_two = "Monster is too dead to retaliate"
+            const newLog2 = updateBattleEvents(turn_two)
+            setBattleLog({log:newLog2})
+                
+        }
     }
 
     function updateBattleEvents(event){
@@ -56,45 +84,49 @@ const Game = (props)=>{
     }
 
     function damagePlayer(dmg){
-        const hp = character.hitPoints
+        let hp = player.character.hitPoints
         if(dmg < hp ){
             hp = hp - dmg
         } else{
             hp = 0
         }
-        setCharacter({hitPoints:hp})
+        let character = player.character
+        character.hitPoints = hp
+        setPlayer({character})
     }
 
     function damageMonster(dmg){
-        const hp = monsters.hitPoints
+        let hp = enemy.monster.hitPoints
         if(dmg < hp ){
             hp = hp - dmg
         } else{
             hp = 0
         }
-        setMonsters({hitPoints:hp})
+        let monster = enemy.monster
+        monster.hitPoints = hp
+        setEnemy({monster})
     }
 
-    if(!monsters) return null
-    if(!character) return null
+    if(!enemy) return null
+    if(!player) return null
     if(!battleLog === undefined) return null
     return (
         <div className="game_screen">
             {console.log("rendering game")}
             <div className="game_screen_top">
                 <BattleLog battleLog={battleLog}/>
-                <Monster monster={monsters}/>
+                <Monster monster={enemy.monster}/>
                 <div className="game_screen_sprites">
                 <div className="game_screen_character_sprites">
-                    <img src={character.image} width="100px" height="100px"/>
+                    <img src={player.character.image} width="100px" height="100px"/>
                 </div>
                 <div className="game_screen_monster_sprites">
-                    <img src={monsters.image} width="100px" height="100px"/>
+                    <img src={enemy.monster.image} width="100px" height="100px"/>
                 </div>
                 </div>
             </div>
             <div className="game_screen_bottom">
-                <Character character={character} />
+                <Character character={player.character} />
                 <PlayerCommands onAttackHandle={attack} />
                 
                 <div className="game_screen_minimap">
